4b70ca6fe7b88c0afbb5e3a6ccd6aab93e780737
unknown <milesdavis71@hotmail.com>
2023-11-15 18:04:10 +0100

    15 Inline images kész

--- src/header/header.js
+++ src/header/header.js
@@ -1,4 +1,6 @@
 import template from './header.html';
+import logo from './logo.png';
+import add from './add.png';
 
 /**
  * header
@@ -7,5 +9,6 @@ const header = document.createElement('div');
 header.className = 'header';
 
 header.innerHTML = template;
-
+header.querySelector('.app-logo').setAttribute('src', logo);
+header.querySelector('.add-icon').setAttribute('src', add);
 document.body.appendChild(header);
