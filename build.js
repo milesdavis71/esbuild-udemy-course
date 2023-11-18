e4674e3d966449d1aa7eda56bce30ceefbfb4b39
Istvan <hevesiistvan71@hotmail.com>
2023-11-18 10:40:24 +0100

    build.js törlése az src-ből, továbbá 26 Add source map kész.

--- build.js
+++ build.js
@@ -2,10 +2,11 @@ import esbuildServe from 'esbuild-serve';
 
 esbuildServe(
   {
-    entryPoints: ['src/index.js'],
+    entryPoints: ['src/index.ts'],
     bundle: true,
     outfile: '/dist/index.js',
     logLevel: 'info',
+    sourcemap: true,
     loader: {
       '.png': 'file',
       '.html': 'text',
